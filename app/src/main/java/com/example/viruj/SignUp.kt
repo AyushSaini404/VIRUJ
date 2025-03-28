625fe0fdfaf21f019cd5d842cfca2d1604a77c9d
AYUSH <ayushsaini1114@gmail.com>
2025-03-28 21:15:08 +0530

    Added SignUp screen

--- app/src/main/java/com/example/viruj/SignUp.kt
+++ app/src/main/java/com/example/viruj/SignUp.kt
@@ -1,28 +1,28 @@
 package com.example.viruj
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import org.w3c.dom.Text
 
-class Login : AppCompatActivity() {
+class SignUp : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_sign_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val text : TextView = findViewById(R.id.textView13)
+        val text: TextView = findViewById(R.id.Login_pg)
         text.setOnClickListener {
-            val intent = Intent(this , SignUp::class.java)
+            val intent = Intent(this, Login::class.java)
             startActivity(intent)
         }
     }
